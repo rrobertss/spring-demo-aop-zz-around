c2531f776fcaac9ba9911da5e9148d91b55693a3
Robert Siwak <rsiwak@gmail.com>
2021-11-30 21:53:03 +0100

    AOP - Around advice

---
+++ src/main/java/pl/rsof/aopdemo/AroundWithLoggerDemoApp.java
@@ -0,0 +1,46 @@
+/*
+ *  Company: RS
+ *  Project: spring-demo-aop-z-after-returning
+ *  Created: 23 lis 2021  22:30:11
+ *  Author:  RS 		
+ */
+package pl.rsof.aopdemo;
+
+import java.util.logging.Logger;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import pl.rsof.aopdemo.service.TrafficFortuneService;
+
+/**
+ * <p>Porada @Around</p><p>23 lis 2021</p>
+ * @author RS
+ *
+ */
+public class AroundWithLoggerDemoApp {
+
+	
+	private static Logger logger = Logger.getLogger(AroundWithLoggerDemoApp.class.getName());
+	
+	
+	public static void main(String[] args) {
+
+		// read Spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		
+		TrafficFortuneService trafficFortuneService = context.getBean("trafficFortuneService", TrafficFortuneService.class);
+		
+		logger.info("ArroundWithLoggerDemoApp is starting");
+		
+		String data = trafficFortuneService.getFortune();
+		
+		logger.info("My fortune is: "+ data);
+		
+		logger.info("ArroundDemoApp is finished");
+		
+		context.close();
+		
+	}
+
+}
